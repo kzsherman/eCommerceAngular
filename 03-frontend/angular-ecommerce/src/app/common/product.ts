6b04fdfe2393b6de88ce96847e4d3fc73ca6e985
Roman Sherstov <rshersto@montgomerycollege.edu>
2021-02-25 05:31:36 -0500

    sets up basic listing of products from DB

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: string;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+}
